3cc0b3adc7c219d3a4d71b1fff6a0b6365079ad7
Juan Carlos Anastasio Salas <L21070315@cdmadero.tecnm.mx>
2025-03-21 21:05:31 -0600

    Chp01 Compile Opt 3 added fun run() Anastasio

--- Kotlin_Stack_Overflow/src/Capitulo_01/Hello_World_03.kt
+++ Kotlin_Stack_Overflow/src/Capitulo_01/Hello_World_03.kt
@@ -3,6 +3,9 @@ package Capitulo_01
 class Apps {
     companion object{
         @JvmStatic fun main(args:Array<String>){
+            Apps.run()
+        }
+        fun run(){
             println("Hello World")
         }
     }
