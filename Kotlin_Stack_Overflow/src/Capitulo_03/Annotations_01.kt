162e3521ba40ecd9a0c19b96bbf4f99d1489da69
Juan Carlos Anastasio Salas <L21070315@cdmadero.tecnm.mx>
2025-03-21 21:41:57 -0600

    Chp 3 Meta-Annotations added ERROR on EXPRESSION

--- Kotlin_Stack_Overflow/src/Capitulo_03/Annotations_01.kt
+++ Kotlin_Stack_Overflow/src/Capitulo_03/Annotations_01.kt
@@ -1,2 +1,9 @@
 package Capitulo_03
 
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.EXPRESSION
+)
+annotation class Strippable
