cb774d4487ad05ef9a4dcb941ce30b2605308a9e
Juan Carlos Anastasio Salas <L21070315@cdmadero.tecnm.mx>
2025-03-21 21:46:20 -0600

    Chp 3 Created annotation class Fancy, used @Target, @Retention @MustbeComented

---
+++ Kotlin_Stack_Overflow/src/Capitulo_03/Annotations_02.kt
@@ -0,0 +1,11 @@
+package Capitulo_03
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.EXPRESSION
+)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class Fancy
