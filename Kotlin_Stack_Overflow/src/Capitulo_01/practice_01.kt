b54000cc860ebec5676bb15bada742237293ad3d
Juan Carlos Anastasio Salas <L21070315@cdmadero.tecnm.mx>
2025-03-21 21:09:17 -0600

    Chp01 Added First Full Example Practice Anastasio

---
+++ Kotlin_Stack_Overflow/src/Capitulo_01/practice_01.kt
@@ -0,0 +1,18 @@
+package Capitulo_01
+
+fun main(args: Array<String>) {
+    println("Enter Two number")
+    var (a, b) = readLine()!!.split(' ')
+    // !! this operator use for NPE(NullPointerException).
+    println("Max number is : ${maxNum(a.toInt(), b.toInt())}")
+}
+fun maxNum(a: Int, b: Int): Int {
+    var max = if (a > b) {
+        println("The value of a is $a");
+        a
+    } else {
+        println("The value of b is $b")
+        b
+    }
+    return max;
+}
